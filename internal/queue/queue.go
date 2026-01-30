b97888b2792cef31e4d37e97abbacc70745cd7bc
lokeshkarthik5 <lokeshkarthic1999@gmail.com>
2026-01-30 16:04:13 +0530

    queue init

--- internal/queue/queue.go
+++ internal/queue/queue.go
@@ -1,2 +1,53 @@
 package queue
 
+import (
+	"context"
+
+	amq "github.com/rabbitmq/amqp091-go"
+)
+
+type Message struct {
+	body     []byte
+	delivery uint64
+}
+
+type Rabbitmq struct {
+	conn    *amq.Connection
+	channel *amq.Channel
+	queue   string
+}
+
+type Config struct {
+	URL        string
+	Exchange   string
+	Queue      string
+	DLQ        string
+	RoutingKey string
+	Prefetch   int
+}
+
+type Queue interface {
+	Enqueue(ctx context.Context, body []byte) error
+	Dequeue(ctx context.Context) (Message, error)
+	Ack(ctx context.Context, msg Message) error
+	Nack(ctx context.Context, msg Message, requeue bool) error
+	Close() error
+}
+
+func New(cfg Config) (*Rabbitmq, error) {
+
+	conn, err := amq.Dial(cfg.URL)
+	if err != nil {
+		return nil, err
+	}
+	ch, err := conn.Channel()
+	if err != nil {
+		conn.Close()
+		return nil, err
+	}
+	return &Rabbitmq{
+		conn:    conn,
+		channel: ch,
+		queue:   cfg.Queue,
+	}, nil
+}
